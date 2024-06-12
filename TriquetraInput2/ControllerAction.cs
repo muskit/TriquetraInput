722313a4ffd87e5368d2b31e9408cecf661b612c
Mutex- <mutex86@zoho.com>
2024-06-12 08:19:14 +0100

    Changed the FindJoystick method to be able to handle the 3 joysticks of the EF24

--- TriquetraInput2/ControllerAction.cs
+++ TriquetraInput2/ControllerAction.cs
@@ -456,7 +456,15 @@ namespace Triquetra.Input
 
             internal static VRJoystick FindJoystick()
             {
-                return GameObject.FindObjectOfType<VRJoystick>(false);
+                var joysticks = GameObject.FindObjectsOfType<VRJoystick>(false);
+
+                var joystick = joysticks.FirstOrDefault();
+                if (joysticks.Length > 1)
+                {
+                    joystick = joysticks.FirstOrDefault(stick => stick.name == "joyInteractable_sideFront");
+                }
+
+                return joystick;
             }
 
             private static bool menuButtonPressed = false;
